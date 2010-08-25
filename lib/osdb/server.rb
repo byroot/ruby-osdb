7ad80f600f61c4b5fcb2f0274701ce204ea7d8e7
Jean Boussier <jean.boussier@feedback20.com>
2010-08-25 16:59:59 +0200

    feature: OSDb::Server now support #login and #logout

--- lib/osdb/server.rb
+++ lib/osdb/server.rb
@@ -1,12 +1,39 @@
 module OSDb
+  
+  class LoginFailed < ::Exception
+  end
+  
   class Server
     
-    def initialize(*args)
-      @client = ::XMLRPC::Client.new(*args)
+    attr_reader :username, :password, :language, :useragent, :client
+    
+    def initialize(options={})
+      @username = options[:username] || ''
+      @password = options[:password] || ''
+      @language = options[:language] || 'en'
+      @useragent = options[:useragent] || 'ruby-osdb v0.1'
+      @client = ::XMLRPC::Client.new(*options.values_at(:host, :path, :port, :proxy_host, :proxy_port, :http_user, :http_password, :use_ssl, :timeout))
+    end
+    
+    def token
+      @token ||= login
+    end
+    
+    def login
+      response = client.call('LogIn', username, password, language, useragent)
+      if response['status'] != '200 OK'
+        raise LoginFailed.new("Failed to login with #{username} : #{password}. Server return code: #{response['status']}")
+      end
+      response['token']
+    end
+    
+    def logout
+      client.call('LogOut', token)
+      @token = nil
     end
     
     def info
-      @client.call('ServerInfo')
+      client.call('ServerInfo')
     end
     
   end
--- spec/osdb/server_spec.rb
+++ spec/osdb/server_spec.rb
@@ -2,9 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe OSDb::Server do
   
-  subject do
-    OSDb::Server.new('api.opensubtitles.org', '/xml-rpc')
+  before :all do
+    @server = OSDb::Server.new(
+      :host => 'api.opensubtitles.org', 
+      :path => '/xml-rpc'
+    )
   end
+  
+  subject { @server }
   
   it 'should respond to #info' do
     hash_including(
@@ -16,6 +21,18 @@ describe OSDb::Server do
       "xmlrpc_version"=>"0.1",
       "xmlrpc_url"=>"http://api.opensubtitles.net/xml-rpc"
     ).should == subject.info
+  end
+  
+  it 'should automatically call #login when token is needed' do
+    subject.instance_variable_get('@token').should be_nil
+    subject.token.should match(/[a-z0-9]{26}/)
+    subject.instance_variable_get('@token').should == subject.token
+  end
+  
+  it 'should clear @login after #logout' do
+    expect{
+      subject.logout
+    }.to change{ subject.instance_variable_get('@token') }.from(instance_of(String)).to(nil)
   end
   
 end
