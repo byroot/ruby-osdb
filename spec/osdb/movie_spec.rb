d257ab35e174cb83a6d590c6c8f7e25134000330
Jean Boussier <jean.boussier@feedback20.com>
2010-09-04 13:30:42 +0200

    fix: movie spec description

--- spec/osdb/movie_spec.rb
+++ spec/osdb/movie_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe OSDb::Server do
+describe OSDb::Movie do
   
   subject do
     OSDb::Movie.new(File.dirname(__FILE__) + '/../fixtures/somemovie.avi')
