9d6c5872ce386a6cca79f02ed643b745c88ada71
Bart Zonneveld <loop@superinfinite.com>
2010-12-03 17:17:37 +0100

    Refactored options methods into OSDb

--- lib/osdb.rb
+++ lib/osdb.rb
@@ -6,4 +6,45 @@ module OSDb
   autoload :Movie,  "#{base_path}/movie"
   autoload :Server, "#{base_path}/server"
   autoload :Sub,    "#{base_path}/sub"
+  
+  def self.parse_options
+    @@options = {:languages => [env_lang.to_iso639_2b], :force => false, :dir => nil, :silent => false }
+    
+    @parser ||= OptionParser.new do |opts|
+      opts.banner = "Automatically download subs for your video files using opensubtitle.org"
+      opts.separator ""
+      opts.separator "Usage: getsub [options] DIRECTORY | VIDEO_FILE [VIDEO_FILE ...]"
+      opts.separator ""
+      opts.separator "Main options:"
+
+      opts.on("-l", "--languages LANGUAGES", "Sub language ISO 963 code separated by comma's like fre or eng. Default: env $LANG (#{env_lang.to_iso639_2b})") do |languages|
+        @@options[:languages] = languages.split(',')
+      end
+
+      opts.on("-d", "--directory DIRECTORY", "Specify a directory to search recursively for movies") do |dir|
+        @@options[:dir] = dir
+      end
+
+      opts.on("-f", "--force", "Download sub even if video already has one") { @@options[:force] = true }
+
+      opts.on("-s", "--silent", "Don't output anything") { options[:silent] = true }
+    end
+    @parser.parse!
+
+    @@options
+  end
+  
+  def self.help
+    @parser.help
+  end
+  
+  def self.options
+    @@options
+  end
+  
+  def self.log(str='')
+    unless options[:silent]
+      puts str
+    end
+  end
 end
