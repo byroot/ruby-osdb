7fd0c86300344eae65e0843f1891366b7d819533
Bart Zonneveld <loop@superinfinite.com>
2010-12-03 17:19:03 +0100

    Refactored curl/wget methods into OSDb

--- lib/osdb/sub.rb
+++ lib/osdb/sub.rb
@@ -5,6 +5,19 @@ module OSDb
   class Sub
     
     attr_reader :url, :format, :language, :rating, :movie_name, :raw_data
+
+    def self.download!(url, local_path)
+      OSDb.log "* download #{url} to #{local_path}"
+      if OSDb.curl_available?
+        %x{ curl -s '#{url}' | gunzip > "#{local_path}" }
+      elsif OSDb.wget_available?
+        %x{ wget -O -quiet - '#{url}' | gunzip > "#{local_path}"}
+      else
+        OSDb.log "Can't found any curl or wget please install one of them or manualy download your sub"
+        OSDb.log url
+      end
+    end
+
     
     def initialize(data)
       @url = URI.parse(data['SubDownloadLink'])
