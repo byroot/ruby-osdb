f27ddf7c1431b06ffa8f8c1872cd21e2356bcb43
Joe Fiorini <joe@joefiorini.com>
2011-10-16 00:57:24 -0400

    Cleanup

--- lib/osdb/server.rb
+++ lib/osdb/server.rb
@@ -1,12 +1,12 @@
 module OSDb
-  
+
   class LoginFailed < ::Exception
   end
-  
+
   class Server
-    
+
     attr_reader :username, :password, :language, :useragent, :client
-    
+
     def initialize(options={})
       @username = options[:username] || ''
       @password = options[:password] || ''
@@ -14,11 +14,11 @@ module OSDb
       @useragent = options[:useragent] || 'ruby-osdb v0.1'
       @client = ::XMLRPC::Client.new(*options.values_at(:host, :path, :port, :proxy_host, :proxy_port, :http_user, :http_password, :use_ssl, :timeout))
     end
-    
+
     def token
       @token ||= login
     end
-    
+
     def login
       response = client.call('LogIn', username, password, language, useragent)
       if response['status'] != '200 OK'
@@ -26,24 +26,24 @@ module OSDb
       end
       response['token']
     end
-    
+
     def logout
       client.call('LogOut', token)
       @token = nil
     end
-    
+
     def check_movie_hash(*hashes)
       client.call('CheckMovieHash', token, hashes)
     end
-    
+
     def search_subtitles(*queries)
       subs = client.call('SearchSubtitles', token, queries)['data']
       subs ? subs.map{ |s| Sub.new(s) } : []
     end
-    
+
     def info
       client.call('ServerInfo')
     end
-    
+
   end
 end
--- spec/osdb/server_spec.rb
+++ spec/osdb/server_spec.rb
@@ -1,19 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe OSDb::Server do
-  
+
   before :all do
     @server = OSDb::Server.new(
-      :host => 'api.opensubtitles.org', 
+      :host => 'api.opensubtitles.org',
       :path => '/xml-rpc',
       :timeout => 60, # OS.org is very very slow ....
       :useragent => 'OS Test User Agent'
     )
   end
-  
+
   subject { @server }
-  
-  let(:expected_info) do 
+
+  let(:expected_info) do
     {
       "website_url"=>"http://www.opensubtitles.org",
       "contact"=>"admin@opensubtitles.org",
@@ -22,7 +22,7 @@ describe OSDb::Server do
       "xmlrpc_url"=>"http://api.opensubtitles.org/xml-rpc"
     }
   end
-  
+
   it 'should respond to #info' do
     info = subject.info
     expected_info.each do |key, value|
@@ -31,21 +31,21 @@ describe OSDb::Server do
     info['seconds'].should be_a(Float)
     info['last_update_strings'].should be_a(Hash)
   end
-  
+
   it 'should automatically call #login when token is needed' do
     subject.instance_variable_get('@token').should be_nil
     subject.token.should match(/[a-z0-9]{26}/)
     subject.instance_variable_get('@token').should == subject.token
   end
-  
+
   it 'should clear @login after #logout' do
     expect{
       subject.logout
     }.to change{ subject.instance_variable_get('@token') }.from(instance_of(String)).to(nil)
   end
-  
+
   describe "#check_movie_hash" do
-    
+
     it 'should identify movie' do
       subject.check_movie_hash('37d0c7d0cfcbe280')['data'].should == {
         "37d0c7d0cfcbe280" => {
@@ -56,11 +56,11 @@ describe OSDb::Server do
         }
       }
     end
-    
+
   end
-  
+
   describe '#search_subtitles' do
-    
+
     it 'can search by hash and size' do
       subs = subject.search_subtitles(:moviehash => 'bd71526264fd8bd9', :moviebytesize => '183406990', :sublanguageid => 'fre')
       subs.should be_a(Array)
@@ -70,7 +70,7 @@ describe OSDb::Server do
         sub.raw_data['MovieName'].should == 'How I Met Your Mother'
       end
     end
-    
+
     it 'can search by imdbid' do
       subs = subject.search_subtitles(:imdbid => "0117500", :sublanguageid => 'fre')
       subs.should be_a(Array)
@@ -80,7 +80,7 @@ describe OSDb::Server do
         sub.raw_data['MovieName'].should == 'The Rock'
       end
     end
-    
+
   end
-  
+
 end
